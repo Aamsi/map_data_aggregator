933f6f1fc6666bc152921e442cf7776319744f62
Aamsi <ismaelouali45@gmail.com>
2020-05-01 23:52:10 +0200

    Create flask app

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+from flask import Flask
+
+app_map = Flask(__name__)
+
+from app import routes
---
+++ app/routes.py
@@ -0,0 +1,8 @@
+from flask import render_template
+from app import app_map
+
+
+@app_map.route('/')
+@app_map.route('/index')
+def index():
+    return render_template('conflict.html')
